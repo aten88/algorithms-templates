a22450e6af53eb47d4058966232d5edf56590433
aten88 <virtuoso_ten@mail.ru>
2023-10-08 19:50:37 +0300

    В работе СОСЕДИ

--- python/sprint1_nonfinals/c.py
+++ python/sprint1_nonfinals/c.py
@@ -1,8 +1,22 @@
 from typing import List, Tuple
 
+
 def get_neighbours(matrix: List[List[int]], row: int, col: int) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
+    result = []
+    if row - 1 >= 0:
+        neighbour_1 = matrix[row - 1][col]
+        result.append(neighbour_1)
+    if col + 1 < len(row):
+        neighbour_2 = matrix[row][col + 1]
+        result.append(neighbour_2)
+    if row + 1 < len(matrix):
+        neighbour_3 = matrix[row + 1][col]
+        result.append(neighbour_3)
+    if col - 1 >= 0:
+        neighbour_4 = matrix[row][col - 1]
+        result.append(neighbour_4)
+    return result
+
 
 def read_input() -> Tuple[List[List[int]], int, int]:
     n = int(input())
@@ -13,6 +27,7 @@ def read_input() -> Tuple[List[List[int]], int, int]:
     row = int(input())
     col = int(input())
     return matrix, row, col
+
 
 matrix, row, col = read_input()
 print(" ".join(map(str, get_neighbours(matrix, row, col))))
