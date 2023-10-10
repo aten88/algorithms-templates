250b4e94d105fcf10cdd7eefc315105c0a72d007
aten88 <virtuoso_ten@mail.ru>
2023-10-10 19:39:55 +0300

    Palindrome and HW Ready

--- python/sprint1_nonfinals/f.py
+++ python/sprint1_nonfinals/f.py
@@ -1,5 +1,7 @@
 def is_palindrome(line: str) -> bool:
-    # Здесь реализация вашего решения
-    pass
+    line = line.lower()
+    line = ''.join(filter(str.isalnum, line))
+    return line == line[::-1]
+
 
 print(is_palindrome(input().strip()))
--- python/sprint1_nonfinals/g.py
+++ python/sprint1_nonfinals/g.py
@@ -1,8 +1,19 @@
 def to_binary(number: int) -> str:
-    # Здесь реализация вашего решения
-    pass
+    if number == 0:
+        return 0
+    result = []
+    divisor = 2
+    while number >= divisor-1:
+        result.append(number % divisor)
+        number = number // divisor
+    result = result[::-1]
+    answer = map(str, result)
+    answer = ''.join(answer)
+    return int(answer)
+
 
 def read_input() -> int:
     return int(input().strip())
+
 
 print(to_binary(read_input()))
