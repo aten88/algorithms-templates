93e5de33c16f9e24ed482bb0ab7a554b5cdc1c2f
aten88 <virtuoso_ten@mail.ru>
2023-10-12 13:08:18 +0300

    factorize Ready

--- python/sprint1_nonfinals/j.py
+++ python/sprint1_nonfinals/j.py
@@ -1,8 +1,20 @@
 from typing import List
 
+
 def factorize(number: int) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
+    factors = []
+    while number % 2 == 0:
+        factors.append(2)
+        number = number // 2
+    for i in range(3, int(number**0.5) + 1, 2):
+        while number % i == 0:
+            factors.append(i)
+            number = number // i
+    if number > 1:
+        factors.append(number)
+
+    return factors
+
 
 result = factorize(int(input()))
 print(" ".join(map(str, result)))
