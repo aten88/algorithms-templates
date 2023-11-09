942efe6a8ca7ed9a9694a552e446d73e92882e46
aten88 <virtuoso_ten@mail.ru>
2023-11-09 15:31:30 +0300

    Надо доделать сортировку слиянием

--- python/sprint3/K/code.py
+++ python/sprint3/K/code.py
@@ -16,27 +16,42 @@ def merge(arr, lf, mid, rg):
         if i_left == len(left):
             result += right[i_right:]
             break
-    return result
+    return sorted(result)
 
 
 def merge_sort(arr, lf, rg):
+    if rg == 1:
+        return arr
     if rg - lf >= 2:
         mid = (lf + rg) // 2
         merge_sort(arr, lf, mid)
         merge_sort(arr, mid, rg)
         arr[lf:rg] = merge(arr, lf, mid, rg)
+        return arr[lf:rg]
 
 
 def test():
-	a = [1, 4, 9, 2, 10, 11]
-	b = merge(a, 0, 3, 6)
-	expected = [1, 2, 4, 9, 10, 11]
-	assert b == expected
-	c = [1, 4, 2, 10, 1, 2]
-	merge_sort(c, 0, 6)
-	expected = [1, 1, 2, 2, 4, 10]
-	assert c == expected
+    a = [1, 4, 9, 2, 10, 11]
+    b = merge(a, 0, 3, 6)
+    expected = [1, 2, 4, 9, 10, 11]
+    assert b == expected
+    c = [1, 4, 2, 10, 1, 2]
+    merge_sort(c, 0, 6)
+    expected = [1, 1, 2, 2, 4, 10]
+    assert c == expected
+
+
+def read_input():
+    command = str(input())
+    n_elements = int(input())
+    array = list(map(int, input().split(' ')))
+    return command, n_elements, array
 
 
 if __name__ == '__main__':
-	test()
+    test()
+    command, n_e, array = read_input()
+    if command == 'sort':
+        print(' '.join(map(str, merge_sort(array, 0, n_e))))
+    if command == 'merge':
+        print(' '.join(map(str, merge(array, 0, n_e//2, n_e))))
