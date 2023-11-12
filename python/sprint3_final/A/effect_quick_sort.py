f746233223de1f8dd971665cf1379e61ddb8fabd
Алексей Тен <virtuoso_ten@mail.ru>
2023-11-12 13:31:52 +0300

    Осталось ускорить работу на 1 сек

--- python/sprint3_final/A/effect_quick_sort.py
+++ python/sprint3_final/A/effect_quick_sort.py
@@ -1,52 +1,30 @@
-import random
-
-alla = [5, 10, 'alla']
-gena = [5, 10, 'gena']
-
-
-def comparsion_elements(arr1, arr2):
-    result = []
-    if arr1[0] > arr2[0]:
-        result.append(arr1[2])
-    if arr1[0] == arr2[0]:
-        if arr1[1] < arr2[1]:
-            result.append(arr1[2])
-        if arr1[1] == arr2[1]:
-            if arr1[2] < arr2[2]:
-                result.append(arr1[2])
-            else:
-                result.append(arr2[2])
-        else:
-            result.append(arr2[2])
-    else:
-        result.append(arr2[2])
-
-    if len(result) > 0:
-        return result[0]
+def custom_sort_key(item):
+    return (-int(item[1]), int(item[2]), item[0])
 
 
 def quick_sort(array):
-    if len(array) > 1:
-        x = int(array[random.randint(0, len(array)-1)][1])
-        highest = [u for u in array if int(u[1]) > x]
-        equivalent = [u for u in array if int(u[1]) == x]
-        low = [u for u in array if int(u[1]) < x]
-        array = quick_sort(highest) + equivalent + quick_sort(low)
-    return array
+    if len(array) <= 1:
+        return array
+    pivot_key = custom_sort_key(array[0])
+    equal = [x for x in array if custom_sort_key(x) == pivot_key]
+    less = [x for x in array if custom_sort_key(x) < pivot_key]
+    greater = [x for x in array if custom_sort_key(x) > pivot_key]
+    return quick_sort(greater) + equal + quick_sort(less)
 
 
 def read_input():
     n = int(input())
-    members_list = []
-    for _ in range(n):
+    members_list = [None] * n
+    for i in range(n):
         member = input().strip().split()
-        members_list.append(list(member))
+        members_list[i] = member
     return n, members_list
 
 
 def test():
     n, member_list = read_input()
-    print(quick_sort(member_list))
+    for member in reversed(quick_sort(member_list)):
+        print(member[0])
 
 
 if __name__ == '__main__':
